7c7f20c82385bf935006476e2abca9ef4c1ae946
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2019-10-21 23:19:21 +0200

    Classe SmsHandler, svolge tutti e tre i compiti: invio, ricezione, interfaccia.

---
+++ app/src/main/java/com/dezen/riccardo/smshandler/SmsHandler.java
@@ -0,0 +1,84 @@
+package com.dezen.riccardo.smshandler;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SmsHandler {
+
+    private SmsManager smsManager;
+    private String scAddress;
+    private PendingIntent sentIntent;
+    private PendingIntent deliveryIntent;
+    private SMSReceiver smsReceiver;
+    private OnSmsReceivedListener listener;
+
+    public SmsHandler(Context context){
+        smsManager = SmsManager.getDefault();
+        scAddress = null;
+        sentIntent = null;
+        deliveryIntent = null;
+        listener = null;
+        smsReceiver = new SMSReceiver();
+    }
+
+    private class SMSReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Sms_test", "BROADCAST");
+            Bundle bundle = intent.getExtras();
+            if(intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) && bundle != null){
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if(pdus != null && pdus.length > 0){
+                    SmsMessage[] messages = new SmsMessage[pdus.length];
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < pdus.length; i++) {
+                        messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        sb.append(messages[i].getMessageBody());
+                    }
+                    if(listener != null) listener.onReceive(messages);
+                }
+            }
+        }
+    }
+
+    public interface OnSmsReceivedListener{
+        void onReceive(SmsMessage[] messages);
+    }
+
+    public void sendSMS(String destination, String message){
+        smsManager.sendTextMessage(destination,scAddress,message,sentIntent,deliveryIntent);
+    }
+
+    public void setScAddress(String scAddress) {
+        this.scAddress = scAddress;
+    }
+
+    public void setSentIntent(PendingIntent sentIntent) {
+        this.sentIntent = sentIntent;
+    }
+
+    public void setDeliveryIntent(PendingIntent deliveryIntent) {
+        this.deliveryIntent = deliveryIntent;
+    }
+
+    public BroadcastReceiver registerReceiver(Context context){
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        context.registerReceiver(smsReceiver,filter);
+        return smsReceiver;
+    }
+
+    public void setListener(OnSmsReceivedListener listener){
+        this.listener = listener;
+    }
+}
