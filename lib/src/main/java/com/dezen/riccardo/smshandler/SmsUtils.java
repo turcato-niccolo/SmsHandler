70246f395219ab6c5b220952abdc5fdfcf2ff769
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2019-11-08 15:56:34 +0100

    Added some methods to SmsUtils
    Added some methods to SmsUtils to allow phone number formatting to given country.
    Added some TODOs
    
    Signed-off-by: Riccardo De Zen <riccardo.dezen@studenti.unipd.it>

--- lib/src/main/java/com/dezen/riccardo/smshandler/SmsUtils.java
+++ lib/src/main/java/com/dezen/riccardo/smshandler/SmsUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.provider.Telephony;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.room.Room;
@@ -15,14 +17,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SmsUtils {
+    //projection to use for SMS Inbox query
     private static String[] inboxProjection = {
             Telephony.Sms._ID,
             Telephony.Sms.ADDRESS,
             Telephony.Sms.SUBJECT,
             Telephony.Sms.BODY
     };
-    public static List<String> getInbox(Context context){
-        List<String> list = new ArrayList<>();
+
+    /**
+     * Method to get SMS Inbox representation. Used for testing and debugging.
+     * @param context the calling context used to get the ContentResolver cursor.
+     * @return a list containing the SMSMessages
+     */
+    public static List<SMSMessage> getInbox(Context context){
+        List<SMSMessage> list = new ArrayList<>();
         String[] selectionArgs = null;
         String selectionClause = null;
         Cursor mCursor = context.getContentResolver().query(
@@ -32,13 +41,8 @@ public class SmsUtils {
         if(mCursor != null && mCursor.getCount() >= 1){
             mCursor.moveToFirst();
             while(!mCursor.isLast()){
-                StringBuilder sb = new StringBuilder()
-                        .append(mCursor.getString(0)).append(" ")
-                        .append(mCursor.getString(1)).append(" ")
-                        .append(mCursor.getString(2)).append(" ")
-                        .append(mCursor.getString(3));
-                list.add(sb.toString());
-                Log.d("SMSUtils", sb.toString());
+                SMSMessage sms = new SMSMessage(new SMSPeer(mCursor.getString(1)),mCursor.getString(3));
+                Log.d("SMSUtils", sms.toString());
                 mCursor.moveToNext();
             }
             mCursor.close();
@@ -46,21 +50,25 @@ public class SmsUtils {
         return list;
     }
 
-
+    /**
+     * Method to log the unread messages in the database.
+     * @param context The calling context, used to instantiate the database.
+     */
     public static void logUnreadMessages(Context context){
-        new LogTask(context).execute();
+        SmsDatabase db = Room.databaseBuilder(context, SmsDatabase.class, SmsHandler.SMS_HANDLER_LOCAL_DATABASE)
+                .enableMultiInstanceInvalidation()
+                .build();
+        new LogTask(db).execute();
     }
 
+    //AsyncTask to access database since it cannot be accessed from main Thread
     private static class LogTask extends AsyncTask<String, Integer, Void>{
-        private Context context;
-        public LogTask(Context context){
-            this.context = context;
+        private SmsDatabase db;
+        public LogTask(SmsDatabase db){
+            this.db = db;
         }
         @Override
         protected Void doInBackground(String... strings) {
-            SmsDatabase db = Room.databaseBuilder(context, SmsDatabase.class, SmsHandler.SMS_HANDLER_LOCAL_DATABASE)
-                    .enableMultiInstanceInvalidation()
-                    .build();
             SmsEntity[] messages = db.access().loadAllSms();
             for(SmsEntity sms : messages){
                 db.access().deleteSms(sms);
@@ -68,5 +76,40 @@ public class SmsUtils {
             }
             return null;
         }
+    }
+
+    /**
+     * Method to get the sim's country code to be used when formatting addresses
+     * @param context the calling context to get the TelephonyManager service.
+     * @return a String containing the country code or null if not available.
+     */
+    public static String getCountryCode(Context context){
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if(tm != null){
+            return tm.getSimCountryIso();
+        }
+        return null;
+    }
+
+    /**
+     * PhoneNumberUtils.formatNumber(...) requires capital letters country code
+     * @param context the calling context to get the TelephonyManager service.
+     * @return a String containing the upper case country code or null if not available.
+     */
+    public static String getCapitalCountryCode(Context context){
+        String countryCode = getCountryCode(context);
+        if(countryCode != null) return countryCode.toUpperCase();
+        return null;
+    }
+
+    /**
+     * Adds the country phone prefix corresponding to the given country Code if not already present
+     * @param number The phone number to be formatted
+     * @param countryCode the countryCode in capital letters
+     * @return The number with the added country code or the original number if the country code cannot be applied
+     */
+    public static String formatSMSNumber(String number, String countryCode){
+        String formattedNumber = PhoneNumberUtils.formatNumberToE164(number,countryCode);
+        return (formattedNumber != null) ? formattedNumber : number;
     }
 }
