e34270ab6543d64d96c7ceb2c274413cb2599fbe
Giorgia Bortoletti <56470846+bortoletti-giorgia@users.noreply.github.com>
2019-11-10 20:04:51 +0100

    Add interface and class to use dictionary

---
+++ networkmanager/src/main/java/com/dezen/riccardo/networkmanager/NetworkDictionaryDatabase.java
@@ -0,0 +1,58 @@
+package com.dezen.riccardo.networkmanager;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import com.dezen.riccardo.networkmanager.database_dictionary.DictionaryDatabase;
+import com.dezen.riccardo.networkmanager.database_dictionary.DictionaryEntity;
+import com.dezen.riccardo.networkmanager.database_dictionary.PeerEntity;
+import com.dezen.riccardo.networkmanager.database_dictionary.ResourceEntity;
+import com.dezen.riccardo.smshandler.Peer;
+import com.dezen.riccardo.smshandler.SMSPeer;
+
+import static com.dezen.riccardo.smshandler.SmsHandler.SMS_HANDLER_LOCAL_DATABASE;
+
+public class NetworkDictionaryDatabase implements DictionaryDb<PeerEntity, ResourceEntity> {
+
+    private DictionaryDatabase db;
+
+    public NetworkDictionaryDatabase(Context context){
+        this.db = Room.databaseBuilder(context, DictionaryDatabase.class, SMS_HANDLER_LOCAL_DATABASE)
+                .enableMultiInstanceInvalidation()
+                .build();
+    }
+
+    @Override
+    public PeerEntity findPeerWithResource(ResourceEntity resource) {
+        PeerEntity peer = db.access().findPeerWithResource(resource.name);
+        return peer;
+    }
+
+    @Override
+    public ResourceEntity[] findResourcesForPeer(PeerEntity peer) {
+        ResourceEntity[] resourcesDb = db.access().findResourcesForPeer(peer.address);
+        return resourcesDb;
+    }
+
+    @Override
+    public PeerEntity[] getAvailablePeers() {
+        return db.access().getAvailablePeers();
+    }
+
+    @Override
+    public ResourceEntity[] getAvailableResource() {
+        return db.access().getAvailableResource();
+    }
+
+    @Override
+    public void add(PeerEntity peer, ResourceEntity[] resources) {
+        for(ResourceEntity resource : resources)
+            db.access().add(new DictionaryEntity(peer.address, resource.name));
+    }
+
+    @Override
+    public void remove(PeerEntity peer) {
+        db.access().remove(peer.address);
+    }
+}
