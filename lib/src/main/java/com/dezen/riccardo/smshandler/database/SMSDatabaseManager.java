1681808b422051fface950c2999a61b3485b635c
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2019-11-30 16:27:43 +0100

    SMSDatabaseManager main Thread support
    - SMSDatabaseManager now supports queries on main Thread.
    - Removed tests cause of errors
    
    Signed-off-by: Riccardo De Zen <riccardo.dezen@studenti.unipd.it>

--- lib/src/main/java/com/dezen/riccardo/smshandler/database/SMSDatabaseManager.java
+++ lib/src/main/java/com/dezen/riccardo/smshandler/database/SMSDatabaseManager.java
@@ -16,6 +16,7 @@ import com.dezen.riccardo.smshandler.SMSPeer;
  * This is not properly a singleton pattern because an on-disk instance and an in-memory instance can
  * coexist.
  * The reason for singleton design pattern is found here https://developer.android.com/training/data-storage/room
+ *
  * @author Riccardo De Zen
  */
 public class SMSDatabaseManager {
@@ -31,20 +32,21 @@ public class SMSDatabaseManager {
     /**
      * Constructor, handles instantiation of the database.
      */
-    private SMSDatabaseManager(Context context, boolean inMemory){
+    private SMSDatabaseManager(Context context, boolean inMemory) {
         //prevent use of reflection to change constructor to public at runtime
-        if(inMemory){
-            if(instance != null)
+        if (inMemory) {
+            if (instance != null)
                 throw new RuntimeException(CON_ERROR);
             database = Room.databaseBuilder(context, SMSDatabase.class, UNREAD_SMS_DB_NAME)
                     .enableMultiInstanceInvalidation()
+                    .allowMainThreadQueries()
                     .build();
-        }
-        else{
-            if(inMemoryInstance != null)
+        } else {
+            if (inMemoryInstance != null)
                 throw new RuntimeException(CON_ERROR);
             database = Room.inMemoryDatabaseBuilder(context, SMSDatabase.class)
                     .enableMultiInstanceInvalidation()
+                    .allowMainThreadQueries()
                     .build();
         }
     }
@@ -52,11 +54,12 @@ public class SMSDatabaseManager {
     /**
      * Method to get the only valid on-disk instance of this class. A new instance is created only if it was
      * null previously. The used context is always the parent application context of the parameter.
+     *
      * @param context The calling context.
      * @return the SMSDatabaseManager instance.
      */
-    public static SMSDatabaseManager getInstance(Context context){
-        if(instance == null){
+    public static SMSDatabaseManager getInstance(Context context) {
+        if (instance == null) {
             instance = new SMSDatabaseManager(context, false);
         }
         return instance;
@@ -64,43 +67,89 @@ public class SMSDatabaseManager {
 
     /**
      * Method to get the only valid on-memory instance of this class.
+     *
      * @param context the calling context.
      * @return the SMSDatabaseManager instance.
      */
-    public static SMSDatabaseManager getInMemoryInstance(Context context){
-        if(inMemoryInstance == null){
+    public static SMSDatabaseManager getInMemoryInstance(Context context) {
+        if (inMemoryInstance == null) {
             inMemoryInstance = new SMSDatabaseManager(context, true);
         }
         return inMemoryInstance;
     }
 
     /**
-     * Method to add SMS to the database. Starts an asynchronous task to do so.
+     * @return the number of stored SMS Messages
+     */
+    public int size(){
+        return database.access().getCount();
+    }
+
+    /**
+     * Method to add one or more SMS to the database, runs on the main Thread
      * @param newMessages the message/messages to be added
      */
     public void addSMS(SMSMessage... newMessages){
-        if(newMessages == null || newMessages.length <= 0) return;
-        new AddSMSTask(database).execute(newMessages);
+        SMSDao dbAccess = database.access();
+        for (SMSMessage message : newMessages) {
+            SMSEntity entity = new SMSEntity(
+                    dbAccess.getCount(),
+                    message.getPeer().getAddress(),
+                    message.getData()
+            );
+            dbAccess.insert(entity);
+        }
     }
+
+    /**
+     * Method to add one or more SMS to the database, runs on the main Thread. Overload for SmsMessage.
+     * @param newMessages the message/messages to be added
+     */
     public void addSMS(SmsMessage... newMessages){
-        if(newMessages == null || newMessages.length <= 0) return;
+        if (newMessages == null || newMessages.length <= 0) return;
         SMSMessage[] messages = new SMSMessage[newMessages.length];
-        for(int i = 0; i < newMessages.length; i++){
+        for (int i = 0; i < newMessages.length; i++) {
             messages[i] = new SMSMessage(newMessages[i]);
         }
         addSMS(messages);
     }
 
-    private static class AddSMSTask extends AsyncTask<SMSMessage,Integer,Boolean>{
+    /**
+     * Method to add SMS to the database. Starts an asynchronous task to do so.
+     *
+     * @param newMessages the message/messages to be added
+     */
+    public void addSMSAsync(SMSMessage... newMessages) {
+        if (newMessages == null || newMessages.length <= 0) return;
+        new AddSMSTask(database).execute(newMessages);
+    }
+
+    /**
+     * Method to add SMS to the database. Starts an asynchronous task to do so. Overload for SmsMessage
+     *
+     * @param newMessages the message/messages to be added
+     */
+    public void addSMSAsync(SmsMessage... newMessages) {
+        if (newMessages == null || newMessages.length <= 0) return;
+        SMSMessage[] messages = new SMSMessage[newMessages.length];
+        for (int i = 0; i < newMessages.length; i++) {
+            messages[i] = new SMSMessage(newMessages[i]);
+        }
+        addSMSAsync(messages);
+    }
+
+    private static class AddSMSTask extends AsyncTask<SMSMessage, Integer, Boolean> {
         private SMSDatabase database;
-        public AddSMSTask (SMSDatabase database){
+
+        public AddSMSTask(SMSDatabase database) {
             this.database = database;
         }
+
         @Override
         protected Boolean doInBackground(SMSMessage... smsMessages) {
-            try{
+            try {
                 SMSDao dbAccess = database.access();
-                for(SMSMessage message : smsMessages){
+                for (SMSMessage message : smsMessages) {
                     SMSEntity entity = new SMSEntity(
                             dbAccess.getCount(),
                             message.getPeer().getAddress(),
@@ -109,8 +158,7 @@ public class SMSDatabaseManager {
                     dbAccess.insert(entity);
                 }
                 return Boolean.TRUE;
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
                 return Boolean.FALSE;
             }
@@ -120,35 +168,37 @@ public class SMSDatabaseManager {
     /**
      * Method to send all unread SMS to the given listener, SMS are deleted once listener returns.
      * Starts an asynchronous task to perform the operations.
+     *
      * @param listener the valid listener
      */
-    public void forwardAllSMS(@NonNull ReceivedMessageListener<SMSMessage> listener){
+    public void forwardAllSMS(@NonNull ReceivedMessageListener<SMSMessage> listener) {
         new ForwardSMSTask(database, listener).execute();
     }
 
-    private static class ForwardSMSTask extends AsyncTask<String,Integer,Boolean>{
+    private static class ForwardSMSTask extends AsyncTask<String, Integer, Boolean> {
         private SMSDatabase database;
         private ReceivedMessageListener<SMSMessage> listener;
-        public ForwardSMSTask (SMSDatabase database, ReceivedMessageListener<SMSMessage> listener){
+
+        public ForwardSMSTask(SMSDatabase database, ReceivedMessageListener<SMSMessage> listener) {
             this.database = database;
             this.listener = listener;
         }
+
         @Override
         protected Boolean doInBackground(String... args) {
             SMSDao dbAccess = database.access();
-            try{
+            try {
                 SMSEntity[] unreadMessages = dbAccess.loadAllSms();
-                for(SMSEntity entity : unreadMessages){
+                for (SMSEntity entity : unreadMessages) {
                     SMSMessage message = new SMSMessage(
                             new SMSPeer(entity.address),
                             entity.body
                     );
-                    if(message.isValid()) listener.onMessageReceived(message);
+                    if (message.isValid()) listener.onMessageReceived(message);
                     dbAccess.deleteSms(entity);
                 }
                 return Boolean.TRUE;
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
                 return Boolean.FALSE;
             }
