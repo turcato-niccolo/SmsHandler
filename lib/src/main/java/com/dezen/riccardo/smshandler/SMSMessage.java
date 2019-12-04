136602a2d2fe9a440614397367abda0f84c8b81d
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2019-12-04 18:02:04 +0100

    Fixed equals method in SMSMessage, after Giorgia Bortoletti's review

--- lib/src/main/java/com/dezen/riccardo/smshandler/SMSMessage.java
+++ lib/src/main/java/com/dezen/riccardo/smshandler/SMSMessage.java
@@ -100,6 +100,10 @@ public class SMSMessage extends Message<String, SMSPeer>{
 
     @Override
     public boolean equals(Object obj){
+        if(obj == null)
+            return false;
+        if(obj == this)
+            return true;
         if(obj instanceof SMSMessage)
             if(((SMSMessage)obj).isValid())
                 return ((SMSMessage)obj).getPeer().equals(this.peer) &&
