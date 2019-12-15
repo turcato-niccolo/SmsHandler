28867a4acd35b674d75eb468685dbf7c0e54e2a7
Giorgia Bortoletti <56470846+bortoletti-giorgia@users.noreply.github.com>
2019-12-15 17:02:49 +0100

    Added nodes routing table (#50)
    * Revert "Merge branch 'feature/distributedNetwork-RoutingTable' into feature/distributedNetwork"
    
    This reverts commit 9c5ef103a673c4739c4dc7fbae47d3a5d438bc74, reversing
    changes made to af5d27fc7e03a86cfd44a2123c2940f97ead538e.
    
    * Implementation of Peers Routing Table
    
    * Updated methods getClosest and changed PeersRoutingTable in NodesRoutingTable
    
    Now NodesRT works with a generic Node. There is only NodesRoutingTable with PeerNode, missed tests with ResourceNode.
    
    * Updated method of comparison between nodes
    
    Now to find distance in NodesRoutingTable it's used position of most significant bit at 1 of BitSet distance XOR.
    
    * Added NodesRoutingTableTest with mock test
    
    In this test distance between two nodes is implementing with method getDistance of PeerNode
    
    * Resolved some specifics after Turcato's review
    
    * Now RoutingTable and KBucket are made of Node<BinarySet>
    
    I have replaced Node with Node<BinarySet> because nodes are based on binary addresses.
    
    * Changed implementation of BinarySet.getFirstPositionOfOne()

---
+++ distributednetworkmanager/src/main/java/com/gruppo1/distributednetworkmanager/NodesRoutingTable.java
@@ -0,0 +1,152 @@
+package com.gruppo1.distributednetworkmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Extends Routing Table with KBucket that contains Node
+ * Size of RoutingTable is fixed and equals to nodeOwner's key length
+ * @author Giorgia Bortoletti
+ */
+public class NodesRoutingTable extends RoutingTable<KBucket> {
+
+    private List<KBucket> bucketsTable;
+    private int sizeTable;
+    private Node<BinarySet> nodeOwner;
+
+    /**
+     * Constructor where the routing table length is equal to nodeOwner length
+     * @param nodeOwner
+     */
+    public NodesRoutingTable(Node<BinarySet>  nodeOwner){
+        new NodesRoutingTable(nodeOwner, nodeOwner.keyLength());
+    }
+
+    /**
+     * Constructor where the routing table length is sizeTable
+     * @param nodeOwner
+     * @param sizeTable dimension of routing table
+     */
+    public NodesRoutingTable(Node<BinarySet> nodeOwner, int sizeTable){
+        this.nodeOwner = nodeOwner;
+        this.sizeTable = sizeTable;
+        bucketsTable = new ArrayList<>(sizeTable);
+        for(int i=0; i<sizeTable; i++)
+            bucketsTable.add(new KBucket(sizeTable));
+    }
+
+    /**
+     * @return nodeOwner of this routing table
+     */
+    public Node getNodeOwner(){ return nodeOwner; }
+
+    /**
+     * @param node Node to add
+     * @return true if the node has been added, false otherwise
+     */
+    @Override
+    public boolean add(Node<BinarySet> node) {
+        if(node == null)
+            return false;
+        return bucketsTable.get(getLocation(node)).add(node);
+    }
+
+    /**
+     * @param node Node to remove
+     * @return true if the node has been removed, false otherwise
+     */
+    @Override
+    public boolean remove(Node<BinarySet> node) {
+        if(node == null)
+            return false;
+        int positionNode = getLocation(node);
+        KBucket bucketFound = bucketsTable.get(positionNode);
+        if(!bucketFound.contains(node))
+            return false;
+        return bucketFound.remove(node);
+    }
+
+    /**
+     * @param node Node of which check presence
+     * @return true if present, false otherwise
+     */
+    @Override
+    public boolean contains(Node<BinarySet> node) {
+        if(node == null || bucketsTable.isEmpty())
+            return false;
+        int positionNode = getLocation(node);
+        if(positionNode == -1)
+            return false;
+        return bucketsTable.get(positionNode).contains(node);
+    }
+
+    /**
+     * @param i index of the bucket in buckets container
+     * @return the bucket at index i, null otherwise
+     */
+    @Override
+    public KBucket getBucket(int i) {
+        try {
+            return bucketsTable.get(i);
+        }catch (IndexOutOfBoundsException e){
+            return null;
+        }
+    }
+
+    /**
+     * @param node Node of the distributed Network
+     * @return the index (between 0 and N -1) of the bucket that maybe containing the given Node with BinarySet, -1 otherwise
+     */
+    @Override
+    public int getLocation(Node<BinarySet> node) {
+        if(node == null)
+            return -1;
+        BinarySet distance = nodeOwner.getDistance(node);
+        return (sizeTable - 1 - distance.getFirstPositionOfOne());
+    }
+
+    /**
+     * @param node
+     * @return the closest Node at the node in the routing table if it is present, null otherwise
+     */
+    @Override
+    public Node getClosest(Node<BinarySet> node) {
+        Node[] nodesClosest = getKClosest(node);
+        if (nodesClosest != null){
+            int minDistance = sizeTable+1;
+            Node nodeClosest = null;
+            for (int i = 0; i < nodesClosest.length; i++) {
+                if(!nodesClosest[i].equals(node)) {
+                    BinarySet distanceBinarySet = node.getDistance(nodesClosest[i]);
+                    int distance = sizeTable - 1 - distanceBinarySet.getFirstPositionOfOne();
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        nodeClosest = nodesClosest[i];
+                    }
+                }
+            }
+            return nodeClosest;
+        }
+        return null;
+    }
+
+    /**
+     * @param node
+     * @return the closest K Nodes at the node in the routing table if it is present, null otherwise
+     */
+    @Override
+    public Node[] getKClosest(Node<BinarySet> node) {
+        int position;
+        if(!node.equals(nodeOwner))
+            position = getLocation(node); //the higher the position, the closer it is
+        else
+            position = sizeTable-1;
+        for (int i = position; i >= 0; i--) { //moves away but it looks for a bucket with some nodes
+            Node[] nodesClosest = bucketsTable.get(i).getElements();
+            if (nodesClosest.length >= 1)
+                return nodesClosest;
+        }
+        return null;
+    }
+
+}
--- distributednetworkmanager/src/main/java/com/gruppo1/distributednetworkmanager/RoutingTable.java
+++ distributednetworkmanager/src/main/java/com/gruppo1/distributednetworkmanager/RoutingTable.java
@@ -9,27 +9,51 @@ package com.gruppo1.distributednetworkmanager;
  * The RoutingTable contains N Bucket(of a defined dimension)
  * i: is the index of a Bucket (between 0 and N-1)
  *
- * The bucket of index i contains the Nodes that have distance (ORX metric) >= 2^(N-i-1) <= 2^(N-i)-1
+ * The bucket of index i contains the nodes that have
+ * 2^(N-i-1) <= (numerical distance XOR metric from node owner)  <= 2^(N-i)-1
+ * and i = N - 1 - (position of most significant bit at 1 of BitSet distance XOR)
  *
  * i.e.
  *
- * if(i = 0)
- *      bit[0] = NOT(myself[0])
- *      bit[0, N-1] = any
+ * if(i == 0)
+ *      bitNode[N-1] = NOT(myself[N-1]) --> bitDistanceXOR[N-1] = 1 --> i = N-1-(N-1) = 0
+ *      bitNode[0, N-2] = any
  *
  * if(i > 0 && i < N-1)
- *      bit[N-1, N-i] = myself[N-1, N-i]
- *      bit[N-(i+1)] = NOT(myself[N-(i+1)]
- *      bit[N-(i+2), 0] = myself[N-(i+2), 0]
+ *      bitNode[N-1, N-i] = myself[N-1, N-i] --> bitDistanceXOR[N-1, N-i] = 0
+ *      bitNode[N-(i+1)] = NOT(myself[N-(i+1)] --> bitDistanceXOR[N-(i+1)] = 1
+ *      bitNode[N-(i+2), 0] = any
  *
  * if(i = N-1)
- *      bit[N-1] = NOT(myself[0])
- *      bit[0, N-1] = myself[0, N-1]
+ *      bitNode[0] = NOT(myself[0]) --> bitDistanceXOR[0] = 1 --> i = N-1-0 = N-1
+ *      bitNode[1, N-1] = myself[1, N-1] --> bitDistanceXOR[1, N-1] = 0
  *      the only Node that has distance = 1
  *
  * @param <B> type of Bucket used for this structure
+ *
+ * @author Niccolò Turcato
+ * @author Giorgia Bortoletti (some fixes)
  */
 public abstract class RoutingTable<B extends Bucket<Node<BinarySet>>> {
+
+    /**
+     * @param node Node to add
+     * @return true if the node has been added, false otherwise
+     */
+    public abstract boolean add(Node<BinarySet> node);
+
+    /**
+     * @param node Node to remove
+     * @return true if the node has been removed, false otherwise
+     */
+    public abstract boolean remove(Node<BinarySet> node);
+
+    /**
+     * @param node Node of which check presence
+     * @return true if present, false otherwise
+     */
+    public abstract boolean contains(Node<BinarySet> node);
+
     /**
      * @param i index of the bucket in buckets container
      * @return the bucket at index i
@@ -43,29 +67,15 @@ public abstract class RoutingTable<B extends Bucket<Node<BinarySet>>> {
     public abstract int getLocation(Node<BinarySet> node);
 
     /**
-     * @return true if the node has been added, false otherwise
+     * @param node
+     * @return the closest Node at the node in the routing table if it is present, otherwise null
      */
-    public abstract boolean add(Node<BinarySet> node);
+    public abstract Node getClosest(Node<BinarySet> node);
 
     /**
-     * @return the closest Node at the ownerNode in the rt if present, otherwise null
+     * @param node
+     * @return the closest K Nodes at the node in the routing table if it is present, otherwise null
      */
-    public abstract Node<BinarySet> getClosest();
-
-    /**
-     * @return the closest K Nodes at the ownerNode in the rt if present, otherwise null
-     */
-    public abstract Node<BinarySet>[] getKClosest();
-
-    /**
-     * @param node node of which check presence in the RT
-     * @return true if present, false otherwise
-     */
-    public abstract boolean contains(Node<BinarySet> node);
-
-    /**
-     * @param node Node to remove form the RT
-     */
-    public abstract boolean remove(Node<BinarySet> node);
+    public abstract Node[] getKClosest(Node<BinarySet> node);
 
 }
